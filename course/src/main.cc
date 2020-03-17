32778c8c8daf67d8cad61e7ccb717b65f5b8adb9
lobotuerk <jtlorente@ekumenlabs.com>
2020-03-17 17:05:48 -0300

    Test finished
    Signed-off-by: lobotuerk <jtlorente@ekumenlabs.com>

--- course/src/main.cc
+++ course/src/main.cc
@@ -4,5 +4,8 @@
 int main(int argc, char **argv) {
   (void) argc;
   (void) argv;
+  const ekumen::math::Isometry t5{ekumen::math::Isometry::RotateAround(ekumen::math::Vector3::kUnitZ, M_PI / 8.)};
+
+  std::cout << t5 << std::endl;
   return 0;
 }
